fc8da37faef64585bb4c46a7a79c55e5578c0944
via-shcherba <via.shcherba@gmail.com>
2019-03-12 13:11:13 +0300

    feat: add implementation of getLoveTrianglesCount()

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ 	let size = preferences.length; 
+	let count = 0;  
+	for (let i=0; i<size; i++) { 
+		let pass1 = preferences[i] - 1;
+		let pass2 = preferences[pass1] - 1;
+		let pass3 = preferences[pass2] - 1;
+		/* if the passage is orderly these are our lovers ))) */
+		if (pass3 == i) count++;			
+	}
+	/* triangle has three lovers / 3 = one love triangle */
+	return Math.floor(count / 3);
 };
